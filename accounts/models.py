36d5fac2780b07cb5ffcd850e3b46ea72ba4beb4
Crossroadsman <crossroadsman@gmail.com>
2018-12-22 13:19:10 -0700

    Start creation of custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class FgoUser(AbstractUser):
+    """The standard User model for the FGODB app.
+
+    The Django docs recommend starting with a custom user model even when
+    you don't expect to need any custom features, since the pain of
+    migrating from the default user model later is so enormous.
+
+    When we might reference `User` directly in the project, we instead use
+    django.contrib.auth.get_user_model() which returns the currently active
+    user model (custom if specified, built-in User otherwise).
+
+    When defining relations in models, specify the custom User model using
+    settings.AUTH_USER_MODEL.
+    See: https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
+    """
+    pass
 
 # Create your models here.
--- fgodb/settings.py
+++ fgodb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'servants',
 ]
 
@@ -81,6 +82,8 @@ DATABASES = {
     }
 }
 
+
+AUTH_USER_MODEL = 'accounts.FgoUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
